30966c22e130f959e4dd23e8ec9152530fc993c3
Issam <149753820+elhadjissam2003@users.noreply.github.com>
2024-03-26 16:26:14 +0000

    main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calcul.h"
+
+
+
+int main() {
+
+    float a,b;
+    scanf("%f%f",&a,&b);
+
+
+        printf("La somme est : %.2f\n",SOMME(a,b));
+        printf("La moyenne est : %.2f\n",MOYENNE(a,b));
+
+        printf("La multiplication est : %.2f",MULTIPLICATION(a,b));
+
+
+
+
+
+return 0;
+
+}
+
+
+
+
