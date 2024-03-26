2aab2e25a851e724a2d738b31eecddb74206ed67
Issam <149753820+elhadjissam2003@users.noreply.github.com>
2024-03-26 16:25:17 +0000

    calcul.c

---
+++ calcul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float SOMME(float a,float b){
+    return a+b;
+}
+float MOYENNE(float a,float b){
+    return (SOMME(a,b))/2;
+}
+float MULTIPLICATION(float a,float b){
+    return a*b;
+}
+
+
+
